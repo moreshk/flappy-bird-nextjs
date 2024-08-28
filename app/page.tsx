d8926411d86e4a733a50583c71fd4575d91b7e72
moreshk <moreshk@gmail.com>
2024-08-28 18:42:21 +0530

    moved icons left bottom

--- app/page.tsx
+++ app/page.tsx
@@ -186,10 +186,13 @@ useEffect(() => {
     <main className="w-full h-screen flex flex-col items-center justify-center bg-black overflow-hidden relative">
       <canvas id="canvas" className="w-full h-full object-contain"></canvas>
       {/* Upgrade icon */}
-      <a href="/upgrade" className="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-10">
-      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white">
-        <path d="M12 19V5M5 12l7-7 7 7"/>
-      </svg>
+      <a href="/upgrade" className="absolute bottom-4 left-4 z-10">
+      <Image
+        src="/upgrade.png"
+        alt="Upgrade"
+        width={48}
+        height={48}
+      />
     </a>
       {/* Debug info - controlled by environment variable */}
       {process.env.NEXT_PUBLIC_SHOW_DEBUG === 'true' && (
--- app/upgrade/page.tsx
+++ app/upgrade/page.tsx
@@ -100,15 +100,16 @@ export default function Upgrade() {
         Upgrade
       </button>
       {message && <p className="mt-4 text-xl">{message}</p>}
-      {/* Navigation icons */}
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-4">
-        <a href="/" className="text-white">
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-          </svg>
-        </a>
-      </div>
+      
+    {/* Navigation icons */}
+    <div className="absolute bottom-4 left-4">
+      <a href="/" className="text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+          <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+          <polyline points="9 22 9 12 15 12 15 22"></polyline>
+        </svg>
+      </a>
+    </div>
     </main>
   );
 }
